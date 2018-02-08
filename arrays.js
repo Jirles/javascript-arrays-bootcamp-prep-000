8c3d82224955459c57b052f99ac53c0a5dc7989e
Emily Jirles <emily.jirles@gmail.com>
2018-02-08 18:36:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,10 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element){
-  pass
+  var newArray = [element...array]
+  return newArray
 }
 
-function 
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element)
+}
